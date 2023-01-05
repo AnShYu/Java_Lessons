cbf015360732e3fe633ec67b6368f482718da8a6
AnShYu <andrey.shirokinskiy@gmail.com>
2023-01-05 20:17:56 +0300

    RunningHistory v.1

---
+++ src/ru/andrey/gym/SortArrayExample.java
@@ -0,0 +1,49 @@
+package ru.andrey.gym;
+
+public class SortArrayExample {
+
+    public <T extends Comparable> void sortArray(T[] array) { // это правильно?
+        int counter;
+        do {
+            counter = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].compareTo(array[i+1]) > 0) {
+                    T currentElement = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = currentElement;
+                    counter++;
+                }
+            }
+        } while (counter > 0);
+    }
+
+    public static void main(String[] args) {
+        Integer[] testArray = {2, 1, 7, 9, 20, 0, 8};
+        SortArrayExample sortArrayExample = new SortArrayExample();
+        sortArrayExample.sortArray(testArray);
+        for (int i = 0; i < testArray.length; i++) {
+            System.out.println(testArray[i] + " ");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+//        if (T instanceof Comparable) {
+//            processBubbleSort(array);
+//        }
+//        else {
+//            throw new NotComparableDataException("Входные данные не реализуют интерфейс Comparable");
+//        }
+//    }
+//
+//    public <T> void processBubbleSort(T[] arr) {
+//
+//    }
+//
+//}
